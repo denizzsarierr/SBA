183146f40e8f4e24f005159fff6a24eadbf73399
denizzsarierr <denizsarier41950@hotmail.com>
2026-03-27 11:01:10 +0100

    Setup CI Pipeline

--- geometry.py
+++ geometry.py
@@ -10,6 +10,11 @@ def circle_area(radius):
     """Calculates the area of a circle.””"""
     return math.pi * (radius ** 2)
 
+def cylinder_surface(radius,height):
+
+    return (2 * math.pi * (radius ** 2)) + (2 * math.pi * radius * height)
+
+
 def rectangle_area(length, width):
     """Calculates the area of a rectangle.
     This PR adds the rectangle area calculation logic.
@@ -23,4 +28,5 @@ if __name__ == "__main__":
     print(f"Triangle Area (b=10, h=5): {triangle_area(10, 5)}")
     print(f"Square Area (a=4): {square_area(4)}")
 
-
+    print(f"Circle Area (r = 6) : {circle_area(6)} ")
+    print(f"Cylinder surface: {cylinder_surface(3,4)}")
---
+++ test_logic.py
@@ -0,0 +1,30 @@
+from geometry import triangle_area,rectangle_area,square_area,circle_area,cylinder_surface
+import pytest
+
+def test_triangle_area():
+
+    assert triangle_area(4,5) == 10
+
+def test_rectangle_area():
+
+    assert rectangle_area(2,5) == 10
+
+def test_square_area():
+
+    assert square_area(6) == 36
+
+def test_circle_area():
+
+    assert circle_area(6) == pytest.approx(113.097, rel=1e-3)
+
+def test_cylinder_surface():
+
+    assert cylinder_surface(3,4) == pytest.approx(131.946, rel=1e-3)
+
+if __name__ == "__main__":
+
+    test_triangle_area()
+    test_rectangle_area()
+    test_circle_area()
+    test_square_area()
+    test_cylinder_surface()
